47c106678955b4b03cb75ce1d81fe0c7f1c4e764
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2023-08-23 15:49:38 -0300

    Server configurado para aceitar um único client e ler uma mensagem

--- chatSocketServer20230822/server/Server.java
+++ chatSocketServer20230822/server/Server.java
@@ -1,15 +1,28 @@
 package server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		
-		ServerSocket srv = new ServerSocket(1234);
+		ServerSocket serverSocket = new ServerSocket(1234);
 		System.out.println("Servidor rodando na porta 1234...");
+		Socket socket = serverSocket.accept();
+		System.out.println("Cliente conectado!");
+		
+		InputStreamReader entrada = new InputStreamReader(socket.getInputStream());
+		BufferedReader leitor = new BufferedReader(entrada);
+		String mensagem;
+		
+		while ((mensagem = leitor.readLine()) != null ) {
+			System.out.println("Mensagem do client: " + mensagem);
+		}
 
 	}
 
