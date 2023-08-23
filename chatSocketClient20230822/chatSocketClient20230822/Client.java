ecb83f93eb1d7ab47a58c2bd44dd97948419cabd
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2023-08-23 16:02:00 -0300

    Client configurado para enviar uma única mensagem

---
+++ chatSocketClient20230822/chatSocketClient20230822/Client.java
@@ -0,0 +1,21 @@
+package chatSocketClient20230822;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+	public static void main(String[] args) throws IOException {
+
+		Socket socket = new Socket("localhost", 1234);
+		Scanner scanner =  new Scanner(System.in);
+		
+		PrintStream saida = new PrintStream(socket.getOutputStream());
+		String teclado = scanner.nextLine();
+		saida.println(teclado);
+
+	}
+
+}
