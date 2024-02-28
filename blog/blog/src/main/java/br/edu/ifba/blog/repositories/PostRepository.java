2907288a5003fd14c60ab954fe051cafd8683791
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2024-02-28 12:43:01 -0300

    limpeza

--- blog/blog/src/main/java/br/edu/ifba/blog/repositories/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifba.blog.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.edu.ifba.blog.models.Post;
-
-public interface PostRepository extends JpaRepository<Post, Long>{
-
-}
