cba966dc88988bf858fa4d5a2c0af56939593569
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2024-02-28 14:26:41 -0300

    commit inicial, cliente enviando msg ao servidor, que exibe msg

---
+++ pweb2024_02_28_sockets_client_server/client/EchoClient.java
@@ -0,0 +1,21 @@
+package client;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class EchoClient {
+
+	public static void main(String[] args) throws UnknownHostException, IOException {
+
+		Socket client = new Socket("localhost", 3333);
+		Scanner leitura = new Scanner(client.getInputStream());
+		PrintWriter escrita = new PrintWriter(client.getOutputStream(), true);
+		
+		escrita.println("Testing, testing, things just got more interesting");
+
+	}
+
+}
---
+++ pweb2024_02_28_sockets_client_server/server/EchoServer.java
@@ -0,0 +1,29 @@
+package server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoServer {
+
+	public static void main(String[] args) throws IOException {
+		
+		int port = 3333;
+		ServerSocket server = new ServerSocket(port);
+		
+		System.out.println("Servidor rodando na porta " + port);
+		
+		while (true) {
+			Socket newClient = server.accept();
+			Scanner leitura = new Scanner(newClient.getInputStream());
+			PrintWriter escrita = new PrintWriter(newClient.getOutputStream(), true);
+			String mensagemRecebida = leitura.nextLine();
+			System.out.println("Mensagem recebida: " + mensagemRecebida);
+		}
+		
+
+	}
+
+}
