d5381c7a48e7486eb596f217893a78eaeb0ee6bf
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2024-03-13 19:57:17 -0300

    Commit inicial

---
+++ pweb2024_03_13_noticias/src/test/java/com/blog/pweb2024_03_13_noticias/NoticiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.pweb2024_03_13_noticias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
